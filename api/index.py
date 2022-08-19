83fc70e2c9ba962cebec3b40bd323f2a237dfe57
Clément POIRET <poiret.clement@outlook.fr>
2022-08-19 13:56:19 +0200

    Initial commi

---
+++ api/index.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Vercel FastAPI template",
+    description="A starter template for FastAPI backends in Vercel deployments",
+    version="0.1.0",
+    docs_url='/api',
+    openapi_url='/api/openapi.json',
+    redoc_url=None,
+    debug=True
+)
+
+
+@app.get('/api/hello')
+async def hello():
+    return {'message': 'Hello world!!'}
+
+@app.get('/')
+async def hello():
+    return {'message': 'Hello world!'}
